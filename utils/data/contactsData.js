99b60f4b443ab8f7297bb1cb948d4d178788a13f
pradipganesh61 <pradipganesh61@gmail.com>
2024-04-24 11:43:42 -0500

    first commit

---
+++ utils/data/contactsData.js
@@ -0,0 +1,11 @@
+export const contactsData = {
+    email: 'pradipganesh61@gmail.com',
+    phone: '+1 (605) 910-1585',
+    address: 'Vermillion, South Dakota, USA - 57069',
+    github: 'https://github.com/pradipganesh61',
+    facebook: 'https://www.facebook.com/pradipganesh61/',
+    linkedIn: 'https://www.linkedin.com/in/pradip-ganesh/',
+    twitter: 'https://www.linkedin.com/in/pradip-ganesh/',
+    stackOverflow: 'https://www.linkedin.com/in/pradip-ganesh/',
+    devUsername: "pradip"
+}
---
+++ utils/data/educations.js
@@ -0,0 +1,22 @@
+export const educations = [
+  {
+    id: 1,
+    title: "Master's Of Science (MS)",
+    duration: "Aug. 2022 - May 2024",
+    major:"Computer Science",
+    institution: "University of South Dakota",
+  },
+  {
+    id: 2,
+    title: "Bachelor of Technology (B.Tech)",
+    duration: "July 2015 - July 2019",
+    major:"Computer Science & Engineering",
+    institution: "Sikkim Manipal University, Sikkim Manipal Institute of Technology (SMIT)",
+  }
+  // {
+  //   id: 3,
+  //   title: "Secondary School Certificate",
+  //   duration: "2008 - 2018",
+  //   institution: "Baitus Saif Islamia Madrasah",
+  // }
+]
