bfd8094b0f8625c5ee9476d0c18995a73db224f5
pradipganesh61 <113200973+pradipganesh61@users.noreply.github.com>
2024-06-03 12:43:10 -0500

    Update experience.js

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -1,24 +1,30 @@
 export const experiences = [
   {
     id: 1,
+    title: 'Software Developer',
+    company: "University of South Dakota",
+    duration: "Oct. 2022 - Mar. 2024"
+  },
+  {
+    id: 2,
     title: 'Senior Software Engineer',
     company: "Infosys Limited",
     duration: "Oct. 2021 - Aug. 2022"
   },
   {
-    id: 2,
+    id: 3,
     title: 'Software Engineer',
     company: "Infosys Limited",
     duration: "Aug. 2019 - Oct. 2021"
   },
   {
-    id: 3,
+    id: 4,
     title: "Software Engineer Intern",
     company: "Infosys Limited",
     duration: "Jan 2019 - May 2019"
   },
   {
-    id: 4,
+    id: 5,
     title: "Summer Intern",
     company: "Manipal Institute of Technology (MIT)",
     duration: "Jun 2017 - July 2017"
