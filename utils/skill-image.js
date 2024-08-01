82688761af2ab8b60f921daf84a996fbd69cac5c
pradipganesh61 <113200973+pradipganesh61@users.noreply.github.com>
2024-08-01 11:32:45 -0400

    Update skill-image.js

--- utils/skill-image.js
+++ utils/skill-image.js
@@ -125,7 +125,7 @@ export const skillsImage = (skill) => {
       return mongoDB;
     case 'mysql':
       return mysql;
-    case 'sql':
+    case 'postgresql':
       return postgresql;
     case 'tailwind':
       return tailwind;
