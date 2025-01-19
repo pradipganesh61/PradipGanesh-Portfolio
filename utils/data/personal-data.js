00465212d0e4722badac3fe775613a00d13442fa
pradipganesh61 <113200973+pradipganesh61@users.noreply.github.com>
2025-01-19 18:09:52 -0500

    Update personal-data.js

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -2,7 +2,7 @@ export const personalData = {
   name: "Pradip Ganesh",
   profile: '/profile.png',
   designation: "Full-Stack Developer",
-  description: "I am Pradip Ganesh, a professional Software Engineer who is passionate and dedicated, and known for a proactive approach to learning and problem-solving. With a keen interest in web application development, particularly Full Stack Developer, I continually seek out opportunities to explore new technologies and contribute to making the web more accessible and functional for all. My expertise lies in leveraging tools such as Java, Spring Boot, TypeScript, AWS, and Microservices to create innovative solutions.",
+  description: "I am Pradip Ganesh, a professional Software Engineer who is passionate and dedicated, and known for a proactive approach to learning and problem-solving. With a keen interest in web application development, particularly Full Stack Developer, I continually seek out opportunities to explore new technologies and contribute to making the web more accessible and functional for all. My expertise lies in leveraging tools such as React, Java, Spring Boot, TypeScript, AWS, and Microservices to create innovative solutions.",
   email: 'pradipganesh90@gmail.com',
   phone: '+1 (703) 659-2898',
   address: 'United States',
