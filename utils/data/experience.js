8bedbf0655ebed0336837d13d3973eb81df1acf5
pradipganesh61 <113200973+pradipganesh61@users.noreply.github.com>
2025-01-19 18:07:05 -0500

    Update experience.js

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -1,36 +1,42 @@
 export const experiences = [
-  {
+   {
     id: 1,
-    title: 'Carta',
+    title: 'Deloitte',
     company: "Software Engineer",
-    duration: "Jul. 2023 - Present"
+    duration: "Sep. 2024 - Present"
   },
   {
     id: 2,
-    title: 'UnitedHealth Group',
-    company: "Software Engineer",
-    duration: "Dec. 2022 - Jun. 2023"
+    title: 'Carta',
+    company: "Full Stack Developer",
+    duration: "Jul. 2023 - Sep. 2024"
   },
   {
     id: 3,
+    title: 'UnitedHealth Group',
+    company: "Full Stack Software Engineer",
+    duration: "Dec. 2022 - Jun. 2023"
+  },
+  {
+    id: 4,
     title: 'Infosys Limited',
     company: "Senior Software Engineer",
     duration: "Oct. 2021 - Aug. 2022"
   },
   {
-    id: 4,
+    id: 5,
     title: 'Infosys Limited',
     company: "Software Engineer",
     duration: "Aug. 2019 - Oct. 2021"
   },
   {
-    id: 5,
+    id: 6,
     title: "Infosys Limited",
     company: "Software Engineer Intern",
     duration: "Jan 2019 - May 2019"
   },
   {
-    id: 6,
+    id: 7,
     title: "Manipal Institute of Technology (MIT)",
     company: "Summer Intern",
     duration: "Jun 2017 - July 2017"
