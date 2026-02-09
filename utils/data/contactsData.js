81f631b2ce9abbf50435bcb4da2c522716df9f3b
pradipganesh61 <113200973+pradipganesh61@users.noreply.github.com>
2026-02-09 00:05:03 -0500

    Update contactsData.js

--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -1,11 +1,11 @@
 export const contactsData = {
-    email: 'pradipganesh90@gmail.com',
-    phone: '+1 (703) 659-2898',
-    address: 'Centreville, Virginia',
+    email: 'pradipganesh61@gmail.com',
+    phone: '+1 (605) 910-1585',
+    address: 'Mechanicsburg, PA',
     github: 'https://github.com/pradipganesh61',
     facebook: 'https://www.facebook.com/pradipganesh61/',
-    linkedIn: 'https://www.linkedin.com/in/pradip-ganesh/',
-    twitter: 'https://www.linkedin.com/in/pradip-ganesh/',
+    linkedIn: 'https://www.linkedin.com/in/pradipganesh61/',
+    twitter: 'https://www.linkedin.com/in/pradipganesh61/',
     stackOverflow: 'https://www.linkedin.com/in/pradip-ganesh/',
     devUsername: "pradip"
 }
